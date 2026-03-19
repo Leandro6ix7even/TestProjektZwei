08048888ef50768ab026729a5a1da8a1930599e5
Leandro6ix7even <LeandroDesimn@gmx.de>
2026-03-19 11:35:51 +0100

    Projektdateien hinzufügen.

---
+++ TestProjekt/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    static void Main()
+    {
+        int result = Add(2, 3);
+
+        if (result == 5)
+        {
+            Console.WriteLine("OK");
+        }
+        else
+        {
+            Console.WriteLine("FEHLER");
+            
+        }
+
+        Console.ReadLine();
+    }
+}
